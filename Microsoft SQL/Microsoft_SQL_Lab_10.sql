a640388a6f0a0f56a54f4aafcaea65a8a05d8942
Iłla Michajłaŭ <62132949+HowlingRot@users.noreply.github.com>
2021-04-12 13:49:51 +0300

    Add files via upload

--- Microsoft SQL/Microsoft_SQL_Lab_10.sql
+++ Microsoft SQL/Microsoft_SQL_Lab_10.sql
@@ -4,27 +4,75 @@
 
 USE DreamHome;
 
---1.Для вычисления количества объектов, проданных в текущем году в заданном, как параметр, городе. Результат вернуть через выходной параметр.
+--1.Для ввода данных в таблицу CONTRACT.
 
-CREATE PROCEDURE numSellProperty (@city shortstring, @counter int OUTPUT) AS
+CREATE PROCEDURE addContract(@Sale_no member_no,@Notary_Office shortstring, @Date_Contract smalldatetime, @Service_Cost money, @Property_no int, @Buyer_no member_no) AS
 BEGIN
-SELECT @counter = count(CONTRACT.Property_no)
-FROM CONTRACT JOIN PROPERTY ON CONTRACT.Property_no = PROPERTY.Property_no
-WHERE DATEDIFF(year, CONTRACT.Date_Contract, GETDATE()) < 1 AND  @city = PROPERTY.City
+INSERT CONTRACT VALUES(@Sale_no,@Notary_Office,@Date_Contract,@Service_Cost,@Property_no,@Buyer_no)
 END
 
 --
 
-DECLARE  @counter int
-DECLARE  @city shortstring
-SET @city = 'Полоцк'
+EXEC addContract 5,'№4','18/03/2020',100000,3000,1
 
-EXEC numSellProperty @city, @counter OUTPUT
+--2.Для повышения заработной платы сотрудника компании 
+--(на 20%, если сотрудник продал одну квартиру в текущем году, на  30%,  если он продал две квартиры в текущем году, на 40%,  если  он продал более двух квартир в текущем году).
+--Входной параметр - Staff_no. Предусмотреть вывод данных о заработной плате до  и после ее повышения. 
 
-PRINT @counter
+CREATE PROCEDURE riseSalary(@Staff_no shortstring) AS
+BEGIN
+SELECT STAFF.Salary as 'old_Salary' FROM STAFF WHERE @Staff_no = STAFF.Staff_no
 
---2.Для выбора объектов собственности, удовлетворяющих требованиям покупателя (в процедуру передать следующие параметры: количество комнат, этаж, общую площадь).
---Квартира должна находиться в том городе, где проживает покупатель.
+DECLARE @up int
+IF 1 = (
+	SELECT count(CONTRACT.Sale_no) 
+	FROM STAFF JOIN PROPERTY ON STAFF.Staff_no = PROPERTY.Staff_no join CONTRACT On PROPERTY.Property_no = CONTRACT.Property_no
+	WHERE @Staff_no = STAFF.Staff_no AND DATEDIFF(year, CONTRACT.Date_Contract, GETDATE()) < 1)
+SET @up = 0.2
+
+IF 2 = (
+	SELECT count(CONTRACT.Sale_no) 
+	FROM STAFF JOIN PROPERTY ON STAFF.Staff_no = PROPERTY.Staff_no join CONTRACT On PROPERTY.Property_no = CONTRACT.Property_no
+	WHERE @Staff_no = STAFF.Staff_no AND DATEDIFF(year, CONTRACT.Date_Contract, GETDATE()) < 1)
+SET @up = 0.3
+
+IF 2 < (
+	SELECT count(CONTRACT.Sale_no) 
+	FROM STAFF JOIN PROPERTY ON STAFF.Staff_no = PROPERTY.Staff_no join CONTRACT On PROPERTY.Property_no = CONTRACT.Property_no
+	WHERE @Staff_no = STAFF.Staff_no AND DATEDIFF(year, CONTRACT.Date_Contract, GETDATE()) < 1)
+SET @up = 0.4
+
+UPDATE STAFF
+SET Salary = Salary + Salary*@up
+WHERE @Staff_no = STAFF.Staff_no
+
+SELECT STAFF.Salary as 'new_Salary' FROM STAFF WHERE @Staff_no = STAFF.Staff_no
+
+END
+
+--
+
+EXEC riseSalary 'BMO550260'
+
+--3.Для вычисления количества отделений компании, в которых среднее число объектов недвижимости, за которые отвечает сотрудник меньше двух.
+--Результат вернуть через выходной параметр.
+
+CREATE PROCEDURE countBranch () AS
+BEGIN
+SELECT count(BRANCH.Branch_no)
+FROM BRANCH as branch1
+GROUP BY BRANCH.Branch_no
+HAVING avg(SELECT count(PROPERTY.Property_no)
+	FROM BRANCH join STAFF on BRANCH.Branch_no = STAFF.Branch_no join PROPERTY ON STAFF.Staff_no = PROPERTY.Staff_no
+	WHERE BRANCH.Branch_no = branch1.Branch_no
+	GROUP BY STAFF.Staff_no) < 2
+END
+
+--
+
+EXEC countBranch 
+
+--4.Для выбора объектов собственности, удовлетворяющих требованиям покупателя(в процедуру передать следующие параметры: количество комнат, этаж, площадь).
 
 CREATE PROCEDURE choiceProperty (@room smallint, @floor smallint, @the_area shortstring, @buyer_no member_no) AS
 BEGIN
@@ -37,31 +85,151 @@ END
 
 EXEC choiceProperty 2, 2, '81/29/9', 1 
 
---3.Для повышения заработной сотрудника на заданный, как параметр, процент при условии наличия у него проданных объектов недвижимости (в процедуру передаётся номер сотрудника, процент повышения заработной платы).
+--5.Для повышения заработной платы сотрудника на заданный, как параметр, процент при условии, что его заработная плата является минимальной в своем отделении(в процедуру передаётся номер сотрудника, процент повышения заработной платы).
+--Вывести сообщение о результате выполнения операции. 
 --Вызвать процедуру для сотрудника с заданным Staff_no.
 
-CREATE PROCEDURE SalaryUp (@staff_no shortstring, @up smallint) AS
+CREATE PROCEDURE riseMinSalary (@staff_no shortstring, @up smallint) AS
 BEGIN
 UPDATE STAFF
-SET Salary = Salary + Salary*@up
-FROM STAFF JOIN PROPERTY ON STAFF.Staff_no = PROPERTY.Staff_no JOIN CONTRACT on CONTRACT.Property_no = PROPERTY.Property_no
-WHERE @staff_no = STAFF.Staff_no
+SET Salary = Salary + Salary*(@up/100)
+FROM STAFF
+WHERE @staff_no = STAFF.Staff_no and STAFF.Salary = (
+	SELECT TOP 1 STAFF.Salary 
+	FROM STAFF JOIN BRANCH on STAFF.Branch_no = BRANCH.Branch_no 
+	WHERE @staff_no = BRANCH.Branch_no
+	ORDER BY num ASC)
 END
 
 --
 
-EXEC SalaryUp BMO550262, 10
+EXEC riseMinSalary BMO550262, 10
 
---4.Для снижения стоимости квартир, которые не были проданы в течение трех месяцев с даты их регистрации (снизить на заданный, как параметр, процент). 
+--6.Для вывода списка сотрудников заданного отделения, которые не продали ни одной квартиры в заданном интервале времени(передать в процедуру дату начала и конца интервала).
 
-CREATE PROCEDURE PriceDown (@down smallint) AS
+CREATE PROCEDURE ListBadStaff (@start smalldatetime, @end smalldatetime) AS
+BEGIN
+SELECT STAFF.Staff_no
+FROM  STAFF JOIN PROPERTY ON STAFF.Staff_no = PROPERTY.Staff_no join CONTRACT On PROPERTY.Property_no = CONTRACT.Property_no
+WHERE CONTRACT.Date_Contract < @start or CONTRACT.Date_Contract > @end 
+GROUP BY STAFF.Staff_no, CONTRACT.Sale_no
+HAVING count( CONTRACT.Sale_no)=0
+END
+
+--
+
+EXEC ListBadStaff 2, 2, '81/29/9', 1 
+
+
+--7.Для снижения цены квартиры на заданный как параметр процент, если квартира была осмотрена покупателями более двух раз, но не была куплена.
+--Предусмотрите вывод списка квартир, цены которых были понижены(с указанием цены после снижения).
+
+CREATE PROCEDURE decreasePrice (@down smallint) AS
 BEGIN
 UPDATE PROPERTY
 SET Selling_Price = Selling_Price - Selling_Price*@down
-FROM PROPERTY JOIN CONTRACT on CONTRACT.Property_no = PROPERTY.Property_no
-WHERE CONTRACT.Sale_no is null AND DATEDIFF(month, PROPERTY.Data_registration, GETDATE()) < 3
+FROM PROPERTY JOIN VIEWING on PROPERTY.Property_no = VIEWING.Property_no LEFT JOIN CONTRACT ON CONTRACT.Property_no = PROPERTY.Property_no
+WHERE CONTRACT.Sale_no is NULL
+GROUP BY Property_no
+HAVING count(Property_no)>1
 END
 
 --
 
-EXEC PriceDown 10
+EXEC decreasePrice
+
+--8.Для удаления из базы данных информации об определенном владельце недвижимости.
+--Если с данных владельцем связаны записи в подчиненных таблицах, удаление должно быть отменено.
+--Вывести сообщение о результате выполнения операции.
+
+CREATE PROCEDURE deleteOwner (@Owner_no member_no) AS
+BEGIN
+DELETE OWNER 
+WHERE Owner_no = @Owner_no
+END
+
+--
+
+EXEC deleteOwner 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--го пить пиво :)))
